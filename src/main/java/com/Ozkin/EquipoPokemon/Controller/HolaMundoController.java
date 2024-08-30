c20fb01a9799b764d58a833253675e9d3e63eab5
Oscar <ozkin1vs@hotmail.com>
2024-08-30 17:07:02 -0500

    aprendiendo inyeccion dependencias

--- src/main/java/com/Ozkin/EquipoPokemon/Controller/HolaMundoController.java
+++ src/main/java/com/Ozkin/EquipoPokemon/Controller/HolaMundoController.java
@@ -1,16 +1,30 @@
 package com.Ozkin.EquipoPokemon.Controller;
 
 
+import com.Ozkin.EquipoPokemon.pokemon.pokemonservice.IPokemonService;
+import com.Ozkin.EquipoPokemon.pokemon.pokemonservice.PokemonServiceImp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HolaMundoController {
 
+    private IPokemonService iPokemonService;
+
+    public HolaMundoController(IPokemonService iPokemonService){
+        this.iPokemonService = iPokemonService;
+    }
+
 
     @GetMapping("/hola")
     public String getHolaMundo(){
         return "Hola mundo";
     }
+    @GetMapping("/inyeccion")
+    public String getDependecia(){
+        return iPokemonService.mensejeInjection();
+    }
+
+
 
 }
